3408796e3025a5c71649a8a74585019cb371c866
Nicholas Olson <nicholasjamesolson@gmail.com>
2015-11-16 16:33:58 -0600

    connected app to google API

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('Ping', [
-    'ionic', 
+    'ionic','ionic.service.core', 
     'Ping.app.controllers',
     'Ping.app.directives',
     'Ping.app.services' 
--- www/js/app/app.controllers.js
+++ www/js/app/app.controllers.js
@@ -3,6 +3,17 @@
 
 angular.module('Ping.app.controllers', [])
 
+.run(function($ionicPlatform){
+  $ionicPlatfrom.ready(function() {
+    var push = new Ionic.push({
+      "debug": true 
+    });
+    push.register(function(token) {
+      console.log("Device token: ", token.token);
+    });
+  });
+});
+
 .controller('PingsController', function($scope){
   $scope.sendPing = function() {
     console.log('test'); 
