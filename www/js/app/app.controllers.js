54972db40d31404c44905802b21796cc491035bc
Nicholas Olson <nicholasjamesolson@gmail.com>
2015-11-23 14:27:42 -0600

    moved .controller code to app.js for a bit

--- www/js/app/app.controllers.js
+++ www/js/app/app.controllers.js
@@ -3,16 +3,18 @@
 
 angular.module('Ping.app.controllers', [])
 
-.run(function($ionicPlatform){
-  $ionicPlatfrom.ready(function() {
-    var push = new Ionic.push({
-      "debug": true 
-    });
-    push.register(function(token) {
-      console.log("Device token: ", token.token);
-    });
-  });
-});
+//.run(function($ionicPlatform){
+//
+//  $ionicPlatfrom.ready(function() {
+//
+//    var push = new Ionic.Push({
+//      "debug": true 
+//    });
+//    push.register(function(token) {
+//      console.log("Device token: ", token.token);
+//    });
+//  });
+//})
 
 .controller('PingsController', function($scope){
   $scope.sendPing = function() {
