4a94cc784e843384586b7f72211124e481d291d2
Gregori Kanatzidis <kanatzidisg@gmail.com>
2016-01-06 21:43:35 -0600

    fetch user profile pics from facebook

--- www/js/app/app.controllers.js
+++ www/js/app/app.controllers.js
@@ -3,7 +3,7 @@
 
 var app = angular.module('Ping.app.controllers', []);
 
-app.controller('PingCtrl', function($scope, $firebaseArray, $firebaseAuth) {
+app.controller('PingCtrl', function($scope, $firebaseArray, $firebaseAuth, $window, $http) {
   var itemsRef = new Firebase("https://shining-heat-1764.firebaseio.com/items");
   var Items = $firebaseArray(itemsRef);
   $scope.items = Items;
@@ -23,13 +23,36 @@ app.controller('PingCtrl', function($scope, $firebaseArray, $firebaseAuth) {
   var usersRef = new Firebase("https://shining-heat-1764.firebaseio.com/users");
   var Auth = $firebaseAuth(usersRef);
 
+  $scope.logout = function() {
+    Auth.$unauth();
+    $window.location.reload(true);
+  };
+
+  $scope.getFriends = function() {
+    var url = 'https://graph.facebook.com/v2.5/me/picture?type=normal&access_token=' + $scope.token;
+    $http.get(url,{responseType: "arraybuffer"}).then(function(res) {
+      console.log(res);
+      var blob = new Blob([res.data], {type: "image/png"});
+      $scope.profile = {
+        'background-image': 'URL("' + URL.createObjectURL(blob) + '")',
+        'background-size': '100% 100%'
+      };
+    }).catch(function(err) {
+      throw err;
+    });
+  };
+
   $scope.login = function() {
-    Auth.$authWithOAuthRedirect("facebook").then(function(authData) {
+    var perms = {
+      scope: 'email,user_friends'
+    };
+    Auth.$authWithOAuthRedirect("facebook", perms).then(function(authData) {
       // User successfully logged in
           console.log(authData);
+          $scope.token = authData.facebook.accessToken;
     }).catch(function(error) {
       if (error.code === "TRANSPORT_UNAVAILABLE") {
-        Auth.$authWithOAuthPopup("facebook").then(function(authData) {
+        Auth.$authWithOAuthPopup("facebook", perms).then(function(authData) {
           // User successfully logged in. We can log to the console
           // since we’re using a popup here
           console.log(authData);
@@ -41,3 +64,4 @@ app.controller('PingCtrl', function($scope, $firebaseArray, $firebaseAuth) {
     });
   };
 });
+
